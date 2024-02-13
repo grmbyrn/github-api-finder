cfd5baf254c55474313be3c5ab6728c65eda2d68
MacBook air <macbookair@MacBook-Air-de-MacBook.local>
2024-02-13 13:24:24 +0100

    connect to github api and display users info on screen

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    process: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
