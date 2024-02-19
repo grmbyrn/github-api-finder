d6cededaf57e6e22a8902b51fc6e07acdf186991
MacBook air <macbookair@MacBook-Air-de-MacBook.local>
2024-02-19 15:32:59 +0000

    add user, repos, and alert

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,36 @@ import About from "./pages/About"
 import NotFound from "./pages/NotFound"
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
+import Alert from "./components/layout/Alert"
+import User from "./pages/User"
 import { GithubProvider } from "./context/github/GithubContext"
+import { AlertProvider } from "./context/alert/AlertContext"
 
 export default function App() {
   return (
     <GithubProvider>
-      <Router>
-        <h1>
-          <div className="flex flex-col justify-between h-screen">
-            <Navbar />
+      <AlertProvider>
+        <Router>
+          <h1>
+            <div className="flex flex-col justify-between h-screen">
+              <Navbar />
 
-            <main className="container mx-auto px-3 pb-12">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/notfound" element={<NotFound />} />
-                <Route path="/*" element={<NotFound />} />
-              </Routes>
-            </main>
+              <main className="container mx-auto px-3 pb-12">
+                <Alert />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/user/:login" element={<User />} />
+                  <Route path="/notfound" element={<NotFound />} />
+                  <Route path="/*" element={<NotFound />} />
+                </Routes>
+              </main>
 
-            <Footer />
-          </div>
-        </h1>
-      </Router>
+              <Footer />
+            </div>
+          </h1>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
